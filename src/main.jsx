cbb394c9ba501c8fb7d498388c75d996dd680cbe
maxtermax <esnene02@gmail.com>
2024-04-29 11:44:07 -0500

    set global hook on message event

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-if (window.parent !== window) {
-  window.__REACT_DEVTOOLS_GLOBAL_HOOK__ =
-    window.parent.__REACT_DEVTOOLS_GLOBAL_HOOK__;
-}
+window.addEventListener('message', event => {
+  if (event.origin.includes("localhost")) {
+    window.__REACT_DEVTOOLS_GLOBAL_HOOK__ = event.data;
+  }
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
