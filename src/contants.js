e2e8e799088787fe75202cb33563ecd047921b8c
maxtermax <esnene02@gmail.com>
2024-04-29 10:27:25 -0500

    Allowing react dev tool

--- src/contants.js
+++
@@ -1,9 +0,0 @@
-export default {
-  FULL_NAME: "fullName",
-  EMAIL: "email",
-  PHONE_NUMBER: "phoneNumber",
-  ADDRESS: "address",
-  ZIP_CODE: "zipCode",
-  USERNAME: "username",
-  PASSWORD: "passowrd",
-};
--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+if (window.parent !== window) {
+  window.__REACT_DEVTOOLS_GLOBAL_HOOK__ =
+    window.parent.__REACT_DEVTOOLS_GLOBAL_HOOK__;
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
