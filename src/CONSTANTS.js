fc58be18f38dd63c456c153332eaf6f594a50bf6
maxtermax <esnene02@gmail.com>
2024-05-04 12:53:13 -0500

    Adding repo_name query param

--- src/CONSTANTS.js
+++ src/CONSTANTS.js
@@ -1,6 +1,10 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const REPO_NAME = urlParams.get('repo_name');
+
 export const CONSTANTS = {
   REPO_OWNER: 'Maxtermax',
-  REPO_NAME: 'hermes-io',
+  REPO_NAME: REPO_NAME || 'file-explorer',
   SET_PAGE: "SET_PAGE",
   SET_FILE_STATE: "SET_FILE_STATE",
   SET_FOLDER_STATE: "SET_FOLDER_STATE",
